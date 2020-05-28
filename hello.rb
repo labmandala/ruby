00be6c16b4d9a5d6a0d0a2657d96419f1c5e336a
sarah <skg@neofilm.us>
2020-05-28 13:36:35 -0400

    Get user input.

--- hello.rb
+++ hello.rb
@@ -1,1 +1,5 @@
-puts "Hello Sarah"
+# prompt user for input
+puts "Enter your name: "
+# get name and chomp off new line after variable
+name = gets.chomp()
+puts ("Hello " + name + ", I love you.")
