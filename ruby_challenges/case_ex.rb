f44ad894b0a23cfc5166bb634eb491e1a70781f3
sarah <skg@neofilm.us>
2020-06-07 16:16:41 -0400

    Case statement example.

---
+++ ruby_challenges/case_ex.rb
@@ -0,0 +1,6 @@
+case("yada")
+    when "raining" then puts "Wear rubber boots!"
+    when "snowing" then puts "Best to stay home where it's warm."
+    when "sunny" then puts "Slap on some sunscreen."
+    else puts "Who knows? Best to prepare for ANYTHING!"
+end 
