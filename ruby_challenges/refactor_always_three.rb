a39c6be36db0a86f272cf8e73595996d6030628b
sarah <skg@neofilm.us>
2020-06-08 13:03:15 -0400

    Clean up code.

--- ruby_challenges/refactor_always_three.rb
+++ ruby_challenges/refactor_always_three.rb
@@ -1,28 +1,21 @@
 # Refactor of Always Three Ruby script
-# Refactor original to use only two variables
+# Refactor to use only two variables
+# Refactor to more efficient code, remove repetition
 
 # 1. begin program by prompting number from user
 puts "Give me a number:"
 
 # 2. grab number from user with gets,
 # transform user's number to integer with .to_i
-# 3. set resulting number to a variable called 'first_number'
+# 3. set resulting number to a variable called 'number'
+# 4. perform math on number
 first_number = gets.to_i
+number = first_number
+first_number += 5
+first_number *= 2
+first_number -= 4
+first_number /= 2
+first_number -= number
 
-# 4. add 5 to first_number
-number = first_number + 5
-
-# 5. multiply by 2
-number = number * 2
-
-# 6. subtract 4
-number = number - 4
-
-# 7. divide by 2
-number = number / 2
-
-# 8. subtract the first number from the final number
-number = number - first_number
-
-# 9. give user the final number
-puts "Final number is Always #{number}"
+# 5. give user the final number
+puts "Final number is Always #{first_number}"
