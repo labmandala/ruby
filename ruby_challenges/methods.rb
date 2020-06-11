aa27aa2a1a007bdfd97d2a0f04b2b1422c9786a8
sarah <skg@neofilm.us>
2020-06-11 01:50:43 -0400

    Write method to accept argument.

---
+++ ruby_challenges/methods.rb
@@ -0,0 +1,21 @@
+# write a method to accept a argument
+
+# define method and put two arguments inside parentheses
+def my_math_method(num1, num2)
+    number = num1 + num2
+    puts number
+end
+
+# call method and pass two numbers in as arguments
+my_math_method(3, 3)
+
+
+
+## example of a basic method:
+
+#def my_math_method
+#    number = 2 + 2
+#    puts number
+#end
+ 
+#my_math_method
