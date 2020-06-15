44236c34016d67be542539157a469019ccae976a
sarah <skg@neofilm.us>
2020-06-15 01:51:31 -0400

    Add third method to personalize.

--- ruby_challenges/method_from_method.rb
+++ ruby_challenges/method_from_method.rb
@@ -1,10 +1,17 @@
 def determine_current_hour
     current_time = Time.new
-    current_time.hour
+    current_hour = current_time.hour
+end
+
+# add a third method asking for users name to personalize greeting
+def determine_username
+	puts "What is your name?"
+	username = gets.strip.to_s
 end
    
-def greeting(name)
+def greeting
     current_hour = determine_current_hour
+    name = determine_username
     if current_hour >= 3 && current_hour < 12
       time = "morning"
     elsif current_hour >= 12 && current_hour < 18
@@ -16,4 +23,4 @@ def greeting(name)
     puts "Good #{time}, #{name.capitalize}!"
 end
    
-greeting("Emily")
+greeting
