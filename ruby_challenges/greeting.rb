431b96e5d1ef6d68e293bf6117c4168c072bb801
sarah <skg@neofilm.us>
2020-06-18 17:17:52 -0400

    Method techniques for greeting.

---
+++ ruby_challenges/greeting.rb
@@ -0,0 +1,19 @@
+# Advanced Method Techniques; improving methods
+
+def greeting(name)
+
+    current_time = Time.new
+    current_hour = current_time.hour
+
+    if(current_hour > 3 && current_hour < 12)
+        time = "morning"
+    elsif(current_hour > 12 && current_hour < 18)
+        time = "afternoon"
+    elsif(current_hour > 18 || current_hour < 2)
+        time = "evening"
+    end
+
+    puts "Good #{time}, #{name.capitalize}!"
+end
+
+greeting("Sarah")
