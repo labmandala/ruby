5cbc3a0bf536508c770a0b00a082454897f52a6c
sarah <skg@neofilm.us>
2020-06-18 17:43:28 -0400

    Add third method to personalize greeting.

--- ruby_challenges/greeting.rb
+++ ruby_challenges/greeting.rb
@@ -7,14 +7,20 @@ def determine_current_hour
     current_hour = current_time.hour
 end
 
-def greeting(name)
+# add a third method asking for user's name to personalize greeting
+def determine_username
+	puts "What is your name?"
+	username = gets.strip.to_s
+end
 
+# use current time to dynamically generate a greeting
+def greeting
     # call defined determine_current_hour method from inside this greeting method
     # save resulting value to current_hour variable
     # this runs the determine_current_hour method and passes data back to greeting method
     # Ruby automatically returns the last value in its method (current_hour)
     current_hour = determine_current_hour
-
+    name = determine_username
     if(current_hour > 3 && current_hour < 12)
         time = "morning"
     elsif(current_hour > 12 && current_hour < 18)
@@ -26,4 +32,4 @@ def greeting(name)
     puts "Good #{time}, #{name.capitalize}!"
 end
 
-greeting("Sarah")
+greeting
