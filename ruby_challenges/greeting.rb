9545d26a0b67ec0be8d9c1a7a48d95e0a5606865
sarah <skg@neofilm.us>
2020-06-18 17:39:35 -0400

    Improve method to be repurposed.

--- ruby_challenges/greeting.rb
+++ ruby_challenges/greeting.rb
@@ -1,9 +1,19 @@
 # Advanced Method Techniques; improving methods
 
-def greeting(name)
-
+# create a stand-alone method that figures out time of day
+# and that can be repurposed outside of greeting method(removed from inside greeting method)
+def determine_current_hour
     current_time = Time.new
     current_hour = current_time.hour
+end
+
+def greeting(name)
+
+    # call defined determine_current_hour method from inside this greeting method
+    # save resulting value to current_hour variable
+    # this runs the determine_current_hour method and passes data back to greeting method
+    # Ruby automatically returns the last value in its method (current_hour)
+    current_hour = determine_current_hour
 
     if(current_hour > 3 && current_hour < 12)
         time = "morning"
