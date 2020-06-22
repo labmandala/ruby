c6dc2f4010d26adea7fd30138801411265e8c891
sarah <skg@neofilm.us>
2020-06-22 14:53:31 -0400

    Convert previous loop to each method.

--- ruby_challenges/loops_iterators.rb
+++ ruby_challenges/loops_iterators.rb
@@ -1,17 +1,4 @@
-## option 1: while loop
-
-#puts “Are you annoyed with me yet? Please answer Y/N:”
-#answer = gets.chomp.downcase
-
-#while (answer == “n”)
-#	puts “Are you annoyed with me yet? Please answer Y/N:”
-#    answer = gets.chomp.downcase
-#end
-
-
-## option 2: iterate a dynamic loop
-## psuedocode: while the number of tweets is less than the total number of tweets,
-## I will keep going
+# convert previous loop to each method
 
 all_tweets = [
 	“My first tweet“,
@@ -20,11 +7,6 @@ all_tweets = [
 	“I have the world’s most boring tweets”
 ]
 
-total_number_of_tweets = all_tweets.size
-tweets_displayed = 0
-
-while (tweets_displayed <= total_number_of_tweets)
-	puts all_tweets[tweets_displayed]
-	tweets_displayed += 1
-	puts tweets_displayed
+all_tweets.each do |tweet|
+	puts tweet
 end
