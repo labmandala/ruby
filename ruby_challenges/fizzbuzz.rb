e8e85421bba79f1e2621bd8220b8231c3cf0b0dc
sarah <skg@neofilm.us>
2020-06-22 15:12:11 -0400

    Create FizzBuzz in Ruby.

---
+++ ruby_challenges/fizzbuzz.rb
@@ -0,0 +1,8 @@
+# write ruby program to print numbers 1 to 100;
+# for multiples of 3, print "Fizz" instead of number,
+# for multiples of 5, print "Buzz" instead and...
+# for numbers which are multiples of both 3 & 5, print "FizzBuzz"
+
+1.upto(100) do |i|
+
+end
