c7593af2eb0ca06fb3587a1c63e4996906dde894
sarah <skg@neofilm.us>
2020-06-22 15:24:12 -0400

    FizzBuzz else print integer, test run.

--- ruby_challenges/fizzbuzz.rb
+++ ruby_challenges/fizzbuzz.rb
@@ -4,11 +4,14 @@
 # for numbers which are multiples of both 3 & 5, print "FizzBuzz"
 
 1.upto(100) do |i|
+    # move this conditional statement to top to be run first, so it's not passed through by others
     if i % 3 == 0 && i % 5  == 0
         puts "FizzBuzz"
     elsif i % 3 == 0
         puts "Fizz"
     elsif i % 5 == 0
         puts "Buzz"
+    else
+        puts 1
     end
 end
