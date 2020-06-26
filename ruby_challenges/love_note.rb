f7d54067b13286c44127cf909f045dfaf3335f46
sarah <skg@neofilm.us>
2020-06-26 12:00:46 -0400

    Love script using a while loop.

---
+++ ruby_challenges/love_note.rb
@@ -0,0 +1,19 @@
+# write a love script using a while loop
+
+def love_note
+	together = 1 + 1
+	alone = 1
+	heat_wave = 0
+	burn = 29
+	while (together >= alone && heat_wave <= burn)
+		heat_wave += 1
+		if (heat_wave == 1)
+			puts "I love you! #{heat_wave} time"
+		elsif
+			puts "I love you! #{heat_wave} times"
+		end
+	end
+	puts "All good things (loops) come to an end"
+end
+
+love_note
