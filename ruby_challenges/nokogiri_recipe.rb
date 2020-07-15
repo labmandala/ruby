2b3e76c45b71448b3664e77588c44d599135093f
sarah <skg@neofilm.us>
2020-07-15 11:20:08 -0400

    Retry recipe with alternate code snippet.

--- ruby_challenges/nokogiri_recipe.rb
+++ ruby_challenges/nokogiri_recipe.rb
@@ -1,18 +1,20 @@
 # scrape a recipe with nokogiri
 
-# require nokogiri gem and one of its dependencies
+# require the nokogiri gem and its dependencies
 require 'nokogiri'
 require 'open-uri'
-
-# get the HTML for the recipe page and assign it to a variable (use single quotes)
-doc = Nokogiri::HTML(open('https://www.marthastewart.com/312598/brick-pressed-sandwich'))
-
-# perform search for class, then grab the list
-list = doc.css('.component-text')
-
-# loop through the elements and puts the content
-list.each do |x|
-    puts x.inner_html
+ 
+doc = Nokogiri::HTML(open('http://www.marthastewart.com/312598/brick-pressed-sandwich'))
+ 
+# search the HTML until you find the right element for the list
+ 
+# get the class with class called 'components-data' that is the span around
+# each ingredient
+list = doc.css('.components-data')
+ 
+# loop through the ingredients and puts just the ingredient text
+list.each do |n|
+    puts n.inner_html
 end
 
-# Error: nokogiri_recipe.rb:8: warning: calling URI.open via Kernel#open is deprecated, call URI.open directly or use URI#ope
+# nokogiri_recipe.rb:7: warning: calling URI.open via Kernel#open is deprecated, call URI.open directly or use URI#open
