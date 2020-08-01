a7cb67e313ec164bb2cb74c7b1cd94c9a26ad516
sarah <skg@neofilm.us>
2020-08-01 01:33:27 -0400

    Convert range to array with .to_a method.

--- bingo-card-creator/bcc.rb
+++ bingo-card-creator/bcc.rb
@@ -18,3 +18,21 @@ bingo_card = <<-BINGO
 BINGO
  
 puts bingo_card
+
+# convert a range into an array, using the .to_a method
+# get random elements from an array using .sample method
+# which in turn takes an optional argument for the amount of elements we want to retrieve
+
+b_col = (1..15).to_a.sample(5)
+i_col = (16..30).to_a.sample(5)
+n_col = (31..45).to_a.sample(4)
+g_col = (46..60).to_a.sample(5)
+o_col = (61..75).to_a.sample(5)
+
+ 
+puts b_col.inspect
+puts i_col.inspect
+puts n_col.inspect
+puts g_col.inspect
+puts o_col.inspect
+
