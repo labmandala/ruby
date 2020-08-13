da444ba933f036999ff8e49672d34db2faf65342
sarah <skg@neofilm.us>
2020-08-12 21:17:39 -0400

    Join a channel on IRC.

--- ruby_challenges/chatbot.rb
+++ ruby_challenges/chatbot.rb
@@ -41,3 +41,28 @@ while message = socket.gets do
   end
   
  end
+
+
+# join a channel on IRC
+ require 'socket'
+ 
+server  = 'moon.freenode.net'
+port    = 6667
+socket = TCPSocket.open(server, port)
+ 
+nickname = 'SkillcrushBotOMG'
+channel  = '#BananaStand123'
+ 
+socket.puts "NICK #{nickname}"
+socket.puts "USER #{nickname} 0 * #{nickname}"
+socket.puts "JOIN #{channel}"
+ 
+while message = socket.gets do
+  puts message
+ 
+  if message.match('^PING :')
+    server = message.split(':').last
+    puts "PONG #{server}"
+    socket.puts "PONG #{server}"
+  end
+end
