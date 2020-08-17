525393c1042eda6225631fb3df5ccf3c4e4a0a06
sarah <skg@neofilm.us>
2020-08-17 09:45:32 -0400

    Interactive messaging.

--- ruby_challenges/chatbot.rb
+++ ruby_challenges/chatbot.rb
@@ -67,3 +67,35 @@ while message = socket.gets do
     socket.puts "PONG #{server}"
   end
 end
+
+
+# add extra branch to the if and check to see if the incoming message has our question:
+#################################
+require 'socket'
+ 
+server  = 'moon.freenode.net'
+port    = 6667
+socket = TCPSocket.open(server, port)
+ 
+nickname = 'SkillcrushBotOMG'
+channel  = '#BananaStand123'
+ 
+socket.puts "NICK #{nickname}"
+socket.puts "USER #{nickname} 0 * #{nickname}"
+socket.puts "JOIN #{channel}"
+ 
+socket.puts "PRIVMSG #{channel} :I am so happy to be here!"
+ 
+while message = socket.gets do
+  puts message
+ 
+  if message.match('^PING :')
+    server = message.split(':').last
+    puts "PONG #{server}"
+    socket.puts "PONG #{server}"
+  elsif message.match('How are you?')
+    puts "PRIVMSG #{channel} :I'm great, thanks!"
+    socket.puts "PRIVMSG #{channel} :I'm great, thanks!"
+  end
+end
+################################
